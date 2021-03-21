9eab5456cbe24988b61690f41799093ee17a3e2a
Matt <mattfsewell@gmail.com>
2021-03-21 13:04:51 -0400

    commit tree 1cd9db13965b61b22b80fcdbcdd0e9a1358c8ae2

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,22 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    state = {
+        reviews: []
+    }
+    componentDidMount(){
+        fetch(URL)
+        .then(r => r.json())
+        .then(data => this.setState({ reviews: data.results }))
+    }
+
+
+    render (){
+        return(
+            <div className="latest-movie-reviews">{this.state.reviews.map(review => <MovieReviews review={review} />)}</div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer
